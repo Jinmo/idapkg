8a43cf5ac3a32fde7c816833cb8edf2373802da1
Jinmo <santoky001@naver.com>
2019-04-30 10:59:24 +0900

    fix bugs 2

--- idapkg.py
+++ idapkg.py
@@ -1,47 +1,6 @@
 import idaapi
-import pkg
 import os
-
-RC = """
-def init_idapkg():
-    "idapythonrc.py is a perfect place to initialize IDAUSR variable"
-    import idaapi
-
-    sys.path.append(os.path.join(idaapi.idadir(), 'plugins'))
-    sys.path.append(os.path.join(idaapi.get_user_idadir(), 'plugins'))
-
-    from pkg.main import init_environment
-    init_environment()
-
-init_idapkg()
-del init_idapkg
-"""
-
-SEP = '\n# idapkg version: ', '# idapkg end\n'
-
-
-def update_pythonrc():
-    rcpath = os.path.join(idaapi.get_user_idadir(), "idapythonrc.py")
-    sep_with_ver = SEP[0] + pkg.__version__
-    payload = '%s\n%s\n%s' % (sep_with_ver, RC.strip(), SEP[1])
-    if os.path.isfile(rcpath):
-        with open(rcpath, 'rb') as f:
-            py = f.read()
-            if payload in py:
-                return
-
-            if all(x in py for x in SEP):
-                py = py.split(SEP[0], 1)
-                py = py[0] + py[1].split(SEP[1], 1)[1]
-            py = payload + py
-            print('Updating idapkg into idapythonrc.py.')
-    else:
-        py = payload
-        print('Added idapkg into idapythonrc.py. Will work after restarting!')
-
-    with open(rcpath, 'wb') as f:
-        f.write(py)
-
+from pkg.main import update_pythonrc
 
 class SkippingPlugin(idaapi.plugin_t):
     flags = 0
@@ -64,4 +23,5 @@ def PLUGIN_ENTRY():
     return SkippingPlugin()
 
 
-update_pythonrc()
+if __name__ == '__main__':
+    update_pythonrc()
--- pkg/main.py
+++ pkg/main.py
@@ -7,6 +7,47 @@ from pkg.logger import logger
 from . import __version__
 
 
+RC = """
+def init_idapkg():
+    "idapythonrc.py is a perfect place to initialize IDAUSR variable"
+    import idaapi
+
+    sys.path.append(os.path.join(idaapi.idadir('plugins')))
+    sys.path.append(os.path.join(idaapi.get_user_idadir(), 'plugins'))
+
+    from pkg.main import init_environment
+    init_environment()
+
+init_idapkg()
+del init_idapkg
+"""
+
+SEP = '\n# idapkg version: ', '# idapkg end\n'
+
+
+def update_pythonrc():
+    rcpath = os.path.join(idaapi.get_user_idadir(), "idapythonrc.py")
+    sep_with_ver = SEP[0] + __version__
+    payload = '%s\n%s\n%s' % (sep_with_ver, RC.strip(), SEP[1])
+    if os.path.isfile(rcpath):
+        with open(rcpath, 'rb') as f:
+            py = f.read()
+            if payload in py:
+                return
+
+            if all(x in py for x in SEP):
+                py = py.split(SEP[0], 1)
+                py = py[0] + py[1].split(SEP[1], 1)[1]
+            py = payload + py
+            print('Updating idapkg into idapythonrc.py.')
+    else:
+        py = payload
+        print('Added idapkg into idapythonrc.py. Will work after restarting!')
+
+    with open(rcpath, 'wb') as f:
+        f.write(py)
+
+
 def init_environment():
     logger.info("idapkg version %s" % __version__)
     prepare_virtualenv(wait=True)
